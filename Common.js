5b6e0184c4690ef7704967315bf541e0d852f336
jason sebek <jabekengineer@gmail.com>
2023-06-19 21:09:13 -0400

    trying to do versioned deployments

--- Common.js
+++ Common.js
@@ -1,1 +1,9 @@
-const helloWorld = () => console.log('hello world');
+function onHomepage(e) {
+
+    var cardHeader = CardService.newCardHeader()
+        .setTitle("Deployment 3")
+
+    var card = CardService.newCardBuilder()
+        .setHeader(cardHeader)
+    return card.build()
+}
